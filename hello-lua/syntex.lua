3bc6e0b019f0e69923ae60dd01f460c4d392b19a
Oliver.W <ole3021@gmail.com>
2017-07-20 14:47:00 +0800

    update lua hello demo

--- hello-lua/syntex.lua
+++ hello-lua/syntex.lua
@@ -44,3 +44,83 @@ print(6.0 * 7.0 == tonumber('42'))  --> true, both are numbers
 -- print(3 & 5) -->  2;    00110b AND 10010b = 00010b.
 -- print(6 | 18) --> 22;    00110b  OR 10010b = 10110b.
 -- print(6 ~ 18) --> 20;    00110b XOR 10010b = 10100b.
+
+--[[Function
+]]
+
+function reduce(a, b)
+  return b, a % b
+end
+
+counter = 1
+
+while counter < 3 do
+  print('>>> counter in while', counter)
+  counter = counter + 1
+end
+
+print('>>> counter after while', counter)
+
+repeat
+  print('>>> counter in repeat', counter)
+  counter = counter + 1
+until counter > 5
+
+for i = 6, 8 do
+  print('>>> for i ', i)
+end
+
+-- TOFIX
+-- for key, value = ipairs({'a', 'b'}) do
+--   print('>>> for key, value', key, value)
+-- end
+
+-- if (0 > 1) do
+--   print('>>> if condition')
+-- elseif 1 > 0 then
+--   print('>>> elseif condition')
+-- else
+--   print('>>> else condition')
+-- end
+
+--[[Flexible number]]
+local a, b, c = 1, 2, 3
+
+a, b = b, a
+
+--[[Table
+Variable]]
+table1 = {aKey = 'aValue'}
+table1.aKey = 'someValue'
+table2 = {key1 = 'value1', ['key2'] = 'value2',
+          [false] = 0, [table1] = table1}
+table2['key2'] = 'bValue'
+
+print('>>> table1', table1.aKey)
+print('>>> table2', table2['key2'])
+
+--[[Lua provides tables may be used as keys
+As ES6 symbles]]
+
+local aTable = {}
+
+local key1   = {}
+aTable[key1] = 100
+
+local key2   = {}
+aTable[key2] = 200
+
+print('>>> aTable[key1]', aTable[key1])
+
+--[[Array in Lua]]
+luaArray = {'human', 'tree'}
+
+print('>>> array, firstItem, length', luaArray, luaArray[1], #luaArray)
+
+first = table.remove(luaArray, 1)
+print('>>> lua array remove first', luaArray[1])
+table.insert(luaArray, 1, first)
+print('>>> lua array add back first', luaArray[1])
+
+table.insert(luaArray, 'raccoon')
+last = table.remove(luaArray, #luaArray)
